402172bfd72b0bc291f71345f7c47adb771df465
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-23 22:26:33 +0300

    home section and nav

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './styles.scss';
+
+import skyscraper from './img/trade.png';
+// import factory from './img/factory.png';
+import home from './img/home.png';
+
+const homeIcon = document.querySelector('#home');
+homeIcon.src = home;
+
+const building = document.querySelector('#skyscraper');
+building.src = skyscraper;
+
+const factoryIcon = document.querySelector('#factory');
+factoryIcon.src = home;
+
+
+
+
